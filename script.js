f73e8fefb1ed745f8635a5e2e07a14a743cd0468
Just-joe924 <theemmanueljohnson@gmail.com>
2026-04-29 14:09:25 +0100

    style: fix navbar

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
-var MainImg = document.getElementById("MainImg");
-var smallimg = document.getElementsByClassName("small-img");
 
-smallimg[0].onclick = function (){
-MainImg.src = smallimg[0].src;
-}
-smallimg[1].onclick = function (){
-MainImg.src = smallimg[1].src;
-}
-smallimg[2].onclick = function (){
-MainImg.src = smallimg[2].src;
-}
-smallimg[3].onclick = function (){
-MainImg.src = smallimg[3].src;
-}
 
 if (bar){
     bar.addEventListener('click', () =>{
