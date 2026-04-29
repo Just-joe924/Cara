f97e3c1a43c52ef632c11eca9ce9d337dc7a4d71
Just-joe924 <theemmanueljohnson@gmail.com>
2026-04-29 13:40:03 +0100

    refactor: rename home page

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function (){
+MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function (){
+MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function (){
+MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function (){
+MainImg.src = smallimg[3].src;
+}
 
 if (bar){
     bar.addEventListener('click', () =>{
@@ -13,3 +28,4 @@ if (close){
         nav.classList.remove('active');
     });
 }
+
